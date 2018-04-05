f1af4e8435b60a42145bd134651219c59a82b6da
peter xing <daxing@usc.edu>
2018-04-04 23:51:16 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 
+import { HeroService } from './hero.service';
+import { MessageService} from './message.service';
+
+import { MessageComponent } from './message/message.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    MessageService,
+    /* . . . */
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
